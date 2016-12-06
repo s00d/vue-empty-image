b52f8a0b1168331765f13cae9cf4392865ed4e31
Pavel <Virus191288@gmail.com>
2016-12-06 18:43:25 +0500

    upload component

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # vue-empty-image
+
+> A reusable focus directive for reusable [Vue.js](https://github.com/vuejs/vue) components
+
+## Overview
+
+Test images for vue
+
+## Requirements
+
+- vue: ^2.0.0
+
+## Install
+
+From npm:
+
+``` sh
+$ npm install vue-empty-image --save
+```
+
+## API
+
+### `empty-image`
+
+A directive that binds focus to the expression in a one-way manner,
+
+``` js
+import { emptyImage } from 'vue-empty-image';
+
+export default {
+  directives: { 'empty-image': emptyImage },
+  template: '<img type="text" v-empty-image="200x200">',
+};
+```
+
+### `mixin`
+
+A mixin that makes the `v-empty-image` directive available to the component under the default name.
+
+``` js
+import { mixin as emptyImage }  from 'vue-empty-image';
+
+export default {
+  mixins: [ emptyImage ],
+  template: '<img type="text" v-empty-image="200x200">',
+};
+```
+
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
---
+++ dist/vue-empty-image.js
@@ -0,0 +1,87 @@
+(function (exports,Vue) { 'use strict';
+
+    Vue = 'default' in Vue ? Vue['default'] : Vue;
+
+    var version = '1.0.0';
+
+    var compatible = (/^2\./).test(Vue.version);
+    if (!compatible) {
+        Vue.util.warn('VueEmptyImage ' + version + ' only supports Vue 2.x, and does not support Vue ' + Vue.version);
+    }
+
+    var config = {
+        text_size: 10,
+        fill_color: '#EEEEEE',
+        text_color: '#AAAAAA'
+    }
+
+    function getTextSize(size) {
+        return Math.max(config.text_size, Math.round([size.h, size.w].sort()[1] / 16))
+    }
+    function drawImage(val, size) {
+        var canvas = document.createElement('canvas')
+        var context = canvas.getContext('2d')
+        var text_size = getTextSize(size)
+        var text = val
+
+        canvas.width = size.w
+        canvas.height = size.h
+        context.fillStyle = config.fill_color
+        context.fillRect(0, 0, size.w, size.h)
+        context.fillStyle = config.text_color
+        context.textAlign = 'center'
+        context.textBaseline = 'middle'
+        context.font = 'bold ' + text_size + 'pt sans-serif'
+
+        if (context.measureText(text).width / size.w > 1) {
+            text_size = config.text_size / (context.measureText(text).width / size.w)
+            context.font = 'bold ' + text_size + 'pt sans-serif'
+        }
+
+        context.fillText(text, size.w / 2, size.h / 2)
+        return canvas.toDataURL("image/png")
+    }
+
+    function bind(el, binding) {
+        var val = binding.value ? binding.value : binding.key
+        var matches = val.match(/^(\d+)x(\d+)$/)
+
+        if (!matches) return
+
+        var size = {w: matches[1], h: matches[2]}
+        el.title = val;
+        el.alt = val
+
+        var dataUrl = drawImage(val, size)
+
+        if (el.tagName === "IMG") {
+            el.src = dataUrl
+        } else {
+            el.style.backgroundImage = 'url("' + dataUrl + '")'
+        }
+    }
+
+    var emptyImage = {
+        bind: bind,
+        componentUpdated: function(el, binding) {
+            if (binding.modifiers.lazy) {
+                if (binding.value === binding.oldValue) return;
+            }
+
+            bind(el, binding)
+        }
+    };
+
+
+
+    var mixin = {
+        directives: {
+            emptyImage: emptyImage,
+        },
+    };
+
+    exports.version = version;
+    exports.emptyImage = emptyImage;
+    exports.mixin = mixin;
+
+})((this.VueEmptyImage = {}),Vue);
